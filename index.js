7d71be69291bbc00fde992c9027f5fc8a3fcc15a
Nick Spangler <spangleyman@mac.com>
2020-05-15 03:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musiciansPlayInstruments = [];
+  for (let num = 0; num < musicians.length; num++) {
+   musiciansPlayInstruments.push(musicians[num] + " plays " + instruments[num]); 
+  }
+  return musiciansPlayInstruments;
+}
+
+function johnLennonFacts(array) {
+  var loudFacts = [];
+  let num = 0
+  while (num < array.length) {
+    console.log("hello")
+  }
+}
+
+function iLoveTheBeatles(number) {
+  var emptyArray = [];
+  do {
+  emptyArray.push("I love the Beatles!");
+  number++;
+      } 
+  while (number < 15);
+  return emptyArray;
+}
